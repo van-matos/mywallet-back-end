5d1d40d5b8a9d7f14571fb22e7bde18b154fab5c
van-matos <vanmatos@poli.ufrj.br>
2022-07-02 16:25:58 -0300

    POST /signup

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
+import joi from "joi";
 
 dotenv.config();
 
@@ -18,13 +19,46 @@ mongoClient.connect().then(()=> {
 
 app.post("/login", async (req, res) => {});
 
-app.post("/signup", async (req, res) => {});
+app.post("/signup", async (req, res) => {
+    const signupSchema = joi.object({
+        name: joi.string().required(),
+        email: joi.string().email().required(),
+        password: joi.string().required(),
+        passConfirm: joi.string().required()
+    });
+
+    const { name, email, password, passConfirm } = req.body;
+
+    const validation = signupSchema.validate(
+        { name, email, password, passConfirm },
+        { abortEarly: false }
+    );
+
+    if (validation.error) {
+        return res.sendStatus(422);
+    }
+
+    if (password !== passConfirm) {
+        return res.sendStatus(403);
+    }
+
+    try {
+        const existingUser= await db.collection("users").findOne({ email });
+
+        if (existingUser) {
+            return res.sendStatus(409);
+        }
+
+        await db.collection("users").insertOne({ name, email, password});
+
+        return res.sendStatus(201);
+    } catch (error) {
+        res.sendStatus(500);
+    }
+});
 
 app.get("/balance", async (req, res) => {});
-
 app.post("/income", async (req, res) => {});
-
 app.post("/expenditure", async (req, res) => {});
 
-app.listen(5000, () => console.log("Server on-line."))
-
+app.listen(5000, () => console.log("Server on-line."));
